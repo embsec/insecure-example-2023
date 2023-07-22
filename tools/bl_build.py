b0186461886fe4cb1e22e5ec79165adc83d766e0
Iv Robinson <arobinsoniv@mitre.org>
2023-07-22 00:11:15 +0000

    chore: Simplify bl_build.py

--- tools/bl_build.py
+++ tools/bl_build.py
@@ -11,52 +11,43 @@ import pathlib
 import shutil
 import subprocess
 
-FILE_DIR = pathlib.Path(__file__).parent.absolute()
+REPO_ROOT = pathlib.Path(__file__).parent.parent.absolute()
+BOOTLOADER_DIR = os.path.join(REPO_ROOT, "bootloader")
 
 
-def copy_initial_firmware(binary_path):
-    """
-    Copy the initial firmware binary to the bootloader build directory
-    Return:
-        None
-    """
-    # Change into directory containing tools
-    os.chdir(FILE_DIR)
-    bootloader = FILE_DIR / '..' / 'bootloader'
-    shutil.copy(binary_path, bootloader / 'src' / 'firmware.bin')
+def copy_initial_firmware(binary_path: str):
+    # Copy the initial firmware binary to the bootloader build directory
+
+    os.chdir(os.path.join(REPO_ROOT, "tools"))
+    shutil.copy(binary_path, os.path.join(BOOTLOADER_DIR, "src/firmware.bin"))
 
 
-def make_bootloader():
-    """
-    Build the bootloader from source.
+def make_bootloader() -> bool:
+    # Build the bootloader from source.
 
-    Return:
-        True if successful, False otherwise.
-    """
-    # Change into directory containing bootloader.
-    bootloader = FILE_DIR / '..' / 'bootloader'
-    os.chdir(bootloader)
+    os.chdir(BOOTLOADER_DIR)
 
-    subprocess.call('make clean', shell=True)
-    status = subprocess.call('make')
+    subprocess.call("make clean", shell=True)
+    status = subprocess.call("make")
 
     # Return True if make returned 0, otherwise return False.
-    return (status == 0)
+    return status == 0
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Bootloader Build Tool')
-    parser.add_argument("--initial-firmware", help="Path to the the firmware binary.", default=None)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Bootloader Build Tool")
+    parser.add_argument(
+        "--initial-firmware",
+        help="Path to the the firmware binary.",
+        default=os.path.join(REPO_ROOT, "firmware/gcc/main.bin"),
+    )
     args = parser.parse_args()
-    if args.initial_firmware is None:
-        binary_path = FILE_DIR / '..' / 'firmware' / 'gcc' / 'main.bin'
-    else:
-        binary_path = os.path.abspath(pathlib.Path(args.initial_firmware))
+    firmware_path = os.path.abspath(pathlib.Path(args.initial_firmware))
 
-    if not os.path.isfile(binary_path):
+    if not os.path.isfile(firmware_path):
         raise FileNotFoundError(
-            "ERROR: {} does not exist or is not a file. You may have to call \"make\" in the firmware directory.".format(
-                binary_path))
+            f'ERROR: {firmware_path} does not exist or is not a file. You may have to call "make" in the firmware directory.'
+        )
 
-    copy_initial_firmware(binary_path)
+    copy_initial_firmware(firmware_path)
     make_bootloader()
