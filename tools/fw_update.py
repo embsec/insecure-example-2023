4f2879cc33294a03e21f0d687877732caacf900c
Iv Robinson <arobinsoniv@mitre.org>
2023-07-23 04:51:24 +0000

    chore!: fw_update now works with the new domain socket setup

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -21,22 +21,28 @@ just a zero
 import argparse
 import struct
 import time
+import socket
 
-from serial import Serial
+from util import DomainSocketSerial
 
-RESP_OK = b'\x00'
+UART0_PATH = "/embsec/UART0"
+UART1_PATH = "/embsec/UART1"
+UART2_PATH = "/embsec/UART2"
+
+RESP_OK = b"\x00"
 FRAME_SIZE = 16
 
 
 def send_metadata(ser, metadata, debug=False):
-    version, size = struct.unpack_from('<HH', metadata)
-    print(f'Version: {version}\nSize: {size} bytes\n')
+    version, size = struct.unpack_from("<HH", metadata)
+    print(f"Version: {version}\nSize: {size} bytes\n")
 
     # Handshake for update
-    ser.write(b'U')
-    
-    print('Waiting for bootloader to enter update mode...')
-    while ser.read(1).decode() != 'U':
+    ser.write(b"U")
+
+    print("Waiting for bootloader to enter update mode...")
+    while ser.read(1).decode() != "U":
+        print("got a byte")
         pass
 
     # Send size and version to bootloader.
@@ -46,7 +52,7 @@ def send_metadata(ser, metadata, debug=False):
     ser.write(metadata)
 
     # Wait for an OK from the bootloader.
-    resp = ser.read()
+    resp = ser.read(1)
     if resp != RESP_OK:
         raise RuntimeError("ERROR: Bootloader responded with {}".format(repr(resp)))
 
@@ -57,7 +63,7 @@ def send_frame(ser, frame, debug=False):
     if debug:
         print(frame)
 
-    resp = ser.read()  # Wait for an OK from the bootloader
+    resp = ser.read(1)  # Wait for an OK from the bootloader
 
     time.sleep(0.1)
 
@@ -68,9 +74,9 @@ def send_frame(ser, frame, debug=False):
         print("Resp: {}".format(ord(resp)))
 
 
-def main(ser, infile, debug):
+def update(ser, infile, debug):
     # Open serial port. Set baudrate to 115200. Set timeout to 2 seconds.
-    with open(infile, 'rb') as fp:
+    with open(infile, "rb") as fp:
         firmware_blob = fp.read()
 
     metadata = firmware_blob[:4]
@@ -79,11 +85,11 @@ def main(ser, infile, debug):
     send_metadata(ser, metadata, debug=debug)
 
     for idx, frame_start in enumerate(range(0, len(firmware), FRAME_SIZE)):
-        data = firmware[frame_start: frame_start + FRAME_SIZE]
+        data = firmware[frame_start : frame_start + FRAME_SIZE]
 
         # Get length of data.
         length = len(data)
-        frame_fmt = '>H{}s'.format(length)
+        frame_fmt = ">H{}s".format(length)
 
         # Construct frame.
         frame = struct.pack(frame_fmt, length, data)
@@ -96,24 +102,36 @@ def main(ser, infile, debug):
     print("Done writing firmware.")
 
     # Send a zero length payload to tell the bootlader to finish writing it's page.
-    ser.write(struct.pack('>H', 0x0000))
+    ser.write(struct.pack(">H", 0x0000))
 
     return ser
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Firmware Update Tool')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Firmware Update Tool")
 
-    parser.add_argument("--port", help="Serial port to send update over.",
-                        required=True)
-    parser.add_argument("--firmware", help="Path to firmware image to load.",
-                        required=True)
-    parser.add_argument("--debug", help="Enable debugging messages.",
-                        action='store_true')
+    parser.add_argument("--firmware", help="Path to firmware image to load.", required=False)
+    parser.add_argument("--debug", help="Enable debugging messages.", action="store_true")
     args = parser.parse_args()
 
-    print('Opening serial port...')
-    ser = Serial(args.port, baudrate=115200, timeout=2)
-    main(ser=ser, infile=args.firmware, debug=args.debug)
+    uart0_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    uart0_sock.connect(UART0_PATH)
 
+    time.sleep(0.2)  # QEMU takes a moment to open the next socket
 
+    uart1_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    uart1_sock.connect(UART1_PATH)
+    uart1 = DomainSocketSerial(uart1_sock)
+
+    time.sleep(0.2)
+
+    uart2_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    uart2_sock.connect(UART2_PATH)
+
+    # Close unused UARTs (if we leave these open it will hang)
+    uart2_sock.close()
+    uart0_sock.close()
+
+    update(ser=uart1, infile=args.firmware, debug=args.debug)
+
+    uart1_sock.close()
