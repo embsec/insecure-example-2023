8b20a476dc6df0bf072ed3f967691475922debc0
Westy251 <Charlie@Beachhurst.com>
2023-07-24 21:42:42 +0000

    Added comments

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2023 The MITRE Corporation. ALL RIGHTS RESERVED
+# Copyright 2023 The MITRE Corporation and team BRUGH!!. ALL RIGHTS RESERVED
 # Approved for public release. Distribution unlimited 23-02181-13.
 
 """
@@ -15,8 +15,8 @@ from  pwn import *
 
 def encrypt(data, key, header):
 
-    cipher = AES.new(key, AES.MODE_GCM)
-    cipher.update(header)
+    cipher = AES.new(key, AES.MODE_GCM)#instantiates an AES object
+    cipher.update(header)#Updates it to use common header (also on Stellaris)
     ciphertext, tag = cipher.encrypt_and_digest(pad(data, 16))#Encrypts the data
     return(ciphertext + cipher.nonce + tag)#Returns encrypted data
 
@@ -30,19 +30,17 @@ def protect_firmware(infile, outfile, version, message, secret):
     #load secret key (256 bits) and header
     key = b""
     header = b""
-
+    #Reads secret_build_output.txt and parses it into the key (32 bytes) and the header (16 bytes)
     with open (secret, "rb") as fp:
         key = fp.readline()
         key = key[0 : len(key) - 1]
         header = fp.readline()
-        print(key)
-        print(header)
 
 
     encrypted = b""
 
     i = 0
-    for i in range (0, len(firmware), 15):#Breaks firmware binary into chunks and runs those chunks through encrypt(). Uses keys from 
+    for i in range (0, len(firmware), 15):#Breaks firmware binary into chunks and runs those chunks through encrypt(). Uses keys from secret_build_output.txt.
         encrypted += encrypt((p8(2) + firmware[i : i + 15]), key, header)
     if (len(firmware) // 15 != 0):
         encrypted += encrypt((p8(2) + firmware[i : len(firmware)]), key, header)
@@ -56,7 +54,7 @@ def protect_firmware(infile, outfile, version, message, secret):
     # Append firmware and message to metadata
     firmware_blob = beginFrame + firmware_and_message
 
-    # Write firmware blob to outfile
+    # Write encrypted firmware blob to outfile
     with open(outfile, 'wb+') as outfile:
         outfile.write(firmware_blob)
 
@@ -70,4 +68,4 @@ if __name__ == '__main__':
     parser.add_argument("--secret", help="path to secret_build_output.txt", required=True)
     args = parser.parse_args()
 
-    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message, secret=args.secret)
+    protect_firmware(infile=args.infile, outfile=args.outfile, version=int(args.version), message=args.message, secret=args.secret)#Calls the firmware protect method
