c761c46a6e30f34f7d01d1db472e1acd88542276
Westy251 <Charlie@Beachhurst.com>
2023-07-25 21:13:02 +0000

    Fixed padding :)

--- tools/fw_protect.py
+++ tools/fw_protect.py
@@ -18,6 +18,7 @@ def encrypt(data, key, header):
     cipher = AES.new(key, AES.MODE_GCM)#instantiates an AES object
     cipher.update(header)#Updates it to use common header (also on Stellaris)
     ciphertext, tag = cipher.encrypt_and_digest(data)#Encrypts the data
+    print("SIZE: " + str(len(ciphertext)))
     return(ciphertext + tag + cipher.nonce)#Returns encrypted data
 
 
@@ -41,14 +42,28 @@ def protect_firmware(infile, outfile, version, message, secret):
 
     i = 0
     for i in range (0, len(firmware), 15):#Breaks firmware binary into chunks and runs those chunks through encrypt(). Uses keys from secret_build_output.txt.
-        encrypted += encrypt((p8(2, endian = "big") + firmware[i : i + 15]), key, header)
+        if ((len(firmware) - i) // 15 != 0):
+            encrypted += encrypt((p8(2, endian = "big") + firmware[i : i + 15]), key, header)
     if (len(firmware) % 15 != 0):
         encrypted += encrypt(pad((p8(2, endian = "big") + firmware[i : len(firmware)]), 16), key, header)
     print(encrypted)
 
     # Append message to end of firmware
-    firmware_and_message = firmware + encrypt(message.encode(), key, header)
+    #firmware_and_message = firmware + encrypt(pad(p8(5, endian = "big") + message.encode(), 16), key, header)
 
+    messageBin = message.encode()
+    messageEncrypted = b""
+    
+    for i in range (0, len(messageBin), 15):#Breaks message into chunks and runs those chunks through encrypt(). Uses keys from secret_build_output.txt.
+        if ((len(messageBin) - i) // 15 != 0):
+            messageEncrypted += encrypt((p8(5, endian = "big") + messageBin[i : i + 15]), key, header)
+        print("Full Message Block")
+        
+    if (len(messageBin) % 15 != 0):#If what's left is too small, it gets broken up and sent
+        messageEncrypted += encrypt(pad((p8(5, endian = "big") + messageBin[i : len(firmware)]), 16), key, header)
+    firmware_and_message = firmware + messageEncrypted
+    
+    
     # Pack message type as a uint8, and version, firmware length and message length as uint16s and encrypts them
     beginFrame = encrypt(pad(p8(1, endian = "big") + p16(version, endian = "big") + p16(len(firmware), endian = "big") + p16(len(message), endian = "big"), 16), key, header)
     #Generates end frame and encrypts it
