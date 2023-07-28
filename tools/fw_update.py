a42a07463fff89dd35e03665a2b85d0800922b77
Westy251 <Charlie@Beachhurst.com>
2023-07-28 15:55:35 +0000

    Fixed grievous error with key generation xD

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -81,6 +81,7 @@ def send_metadata(ser, metadata, debug=False):
     
     #write metadata (START frame)
     ser.write(metadata)
+    print(ser.read(2))
     
     if debug:
         print(metadata)
@@ -181,7 +182,7 @@ if __name__ == "__main__":
     time.sleep(0.2)
 
     uart2_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    uart2_sock.connect(UART2_PATH)
+    #uart2_sock.connect(UART2_PATH)
 
     # Close unused UARTs (if we leave these open it will hang)
     uart2_sock.close()
