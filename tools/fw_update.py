a675a801379959b127cc9fc4b09f64c2df7a8262
Eleanor <ellafan227@gmail.com>
2023-07-31 14:39:36 +0000

    Clean-up fw_update commenting

--- tools/fw_update.py
+++ tools/fw_update.py
@@ -17,88 +17,96 @@ ERROR = b"\x01"
 END = b"\x02"
 
 FRAME_SIZE = 48
-    
+
+# Sends START frame
+# Takes serial object, meta frame, and debug
 def send_metadata(ser, metadata, debug=False):
     # Handshake for update TODO: change frame?
     ser.write(b"U")
 
     print("Waiting for bootloader to enter update mode...")
     while ser.read(1).decode() != "U":
-        print("got a byte")
+        print("Waiting for response...")
         pass
-    print("ok got here")
+    print("Starting upload\n")
     
     send_frame(ser, metadata, debug)
 
-
+# Sends frames
+# Takes serial object, frame, and debug
 def send_frame(ser, frame, debug=False):
-    # Write the DATA frame
-    ser.write(frame) 
-    
+
+    # If debug mode on, prints out frame to be sent
     if debug:
         print_hex(frame)
 
-    #counter for times ERROR was returns
-    falsetimes = 0
-    #boolean that stores whether frame sent was successful
-    framesuccess = False
+    falsetimes = 0 # Error counter
+    failed = True # Stores if sent frame was successful
     
-    #resend frame if given ERROR message
-    while framesuccess == False:
+    # Resend frame if frame fails to send
+    while failed:
+        # Self checks if time-out
         if falsetimes >= 10:
-            raise RuntimeError("invalid frame sent too many times, aborting")
+            raise RuntimeError("Invalid frame sent too many times, aborting")
         
-        # get return message type
-        returnmessagetype = ser.read(1)
-        #get return message info
-        returnmessageinfo = ser.read(1)
+        # Send frame to serial
+        ser.write(frame) 
+        
+        # Get return message type and error number
+        msgType = ser.read(1)
+        errorNum = ser.read(1)
+
+
         time.sleep(0.1)
         
+        # If debug mode on, prints error type
         if debug:
-            print("Resp: {}".format(ord(returnmessageinfo)))
+            print("Resp: {}".format(ord(errorNum)))
             
-        #check for valid return message type
-        if returnmessagetype == b'\x04':
-            if returnmessageinfo == OK:
-                framesuccess = True
-            elif returnmessageinfo == ERROR:
-                ser.write(frame)
-                #increment falsetimes if frame was invalid
-                falsetimes += 1
-            elif returnmessageinfo == END:
-                raise RuntimeError("invalid frame sent, aborting update")
+        # Check message type
+        if msgType == b'\x04':
+            # Check for success
+            if errorNum == OK:
+                failed = False
+            # Check for error
+            elif errorNum == ERROR:
+                falsetimes += 1 # Increment error counter
+            # Check for end
+            elif errorNum == END:
+                raise RuntimeError("Invalid frame sent too many times, aborting")
+            # Check for invalid error
             else:
-                raise RuntimeError("invalid message, aborting update")
+                raise RuntimeError("Invalid error, aborting")
         else:
-            raise RuntimeError("invalid message number, aborting update")
+            raise RuntimeError("Invalid message type, aborting")
 
-
-
+# Sends all frames
+# Takes serial object, encrypted frames location, and debug
+# Returns serial object input
 def update(ser, infile, debug):
     # Open serial port. Set baudrate to 115200. Set timeout to 2 seconds.
     with open(infile, "rb") as fp:
         firmware_blob = fp.read()
 
+    # Send START frame
     metadata = firmware_blob[:48]
-    firmware = firmware_blob[48:]
-
-    #SEND START FRAME
     send_metadata(ser, metadata, debug=debug)
 
-    #SEND DATA FRAMES, MESSAGE FRAME, END FRAME
+    # Send DATA, MESSAGE, and END frames
+    firmware = firmware_blob[48:]
     for idx, frame_start in enumerate(range(0, len(firmware), FRAME_SIZE)):
+        # Chunk and write frames
         data = firmware[frame_start : frame_start + FRAME_SIZE]
-
         send_frame(ser, data, debug=debug)
+        # Confirm frame has been written
         print(f"Wrote frame {idx} ({len(data)} bytes)")
-        
-    print("Wrote end frame")
-    print("Done writing firmware.")
 
+    # Print end message
+    print("Done writing firmware.")
 
     return ser
 
-
+# Carries out program
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Firmware Update Tool")
 
@@ -107,24 +115,29 @@ if __name__ == "__main__":
     parser.add_argument("--debug", help="Enable debugging messages.", action="store_true")
     args = parser.parse_args()
 
+    # Open UART 0
     uart0_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     uart0_sock.connect(UART0_PATH)
 
     time.sleep(0.2)  # QEMU takes a moment to open the next socket
 
+    # Open UART 1
     uart1_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     uart1_sock.connect(UART1_PATH)
     uart1 = DomainSocketSerial(uart1_sock)
 
     time.sleep(0.2)
 
+    # Open UART 2
     uart2_sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
     uart2_sock.connect(UART2_PATH)
 
-    # Close unused UARTs (if we leave these open it will hang)
-    uart2_sock.close()
+    # Close unused UARTs 0 & 2 (if we leave these open it will hang)
     uart0_sock.close()
+    uart2_sock.close()
 
+    # Start updating
     update(ser=uart1, infile=args.firmware, debug=args.debug)
 
+    # Close UART 1
     uart1_sock.close()
